d45287dadac6e80cb81de7599b138f3e77940218
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2024-06-27 12:49:52 +0200

    rv: added auth with jwt

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var todoRoutes = require("./routes/todo.routes")
 var noteRoutes = require("./routes/note.routes")
+var authRoutes = require("./routes/auth.routes")
 var cors = require('cors')
+
+var { protectedMW } = require('./middlewares/authMiddleware')
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,7 +19,12 @@ app.use(cors())
 
 app.use('/api/todos', todoRoutes.router)
 app.use('/api', noteRoutes.router)
+app.use('/auth', authRoutes.router)
 
 app.get('/', (req, res) => res.send('<h1>Welcome to Todo BACKEND</h1>'))
+
+app.get('/dashboard', protectedMW, (req, res) => {
+    res.json(req.user)
+})
 
 module.exports = app;
