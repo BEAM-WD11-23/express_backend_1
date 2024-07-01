a1239258171a9fc44ef3a27a59b87a62abac1f9d
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2024-07-01 14:53:13 +0200

    rv: updated the new frontend url in cors()

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://wd-todo-app-frontend.web.app',
     credentials: true
 }))
 
